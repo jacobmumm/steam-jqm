fd3a5a9ba48a7d0d973d1ba2740cef2dfdd145ad
Jacob Mumm <grendian@gmail.com>
2013-02-05 19:57:14 -0500

    format json

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getJSON("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=B288A76BB1621D6D9F02D6DA5DC54454&steamid=76561197960432907&jsoncallback=",
+    $.getJSON("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=B288A76BB1621D6D9F02D6DA5DC54454&steamid=76561197960432907&jsoncallback=?",
         {format: "json"}, function(data){
            console.log(data); 
         });    
