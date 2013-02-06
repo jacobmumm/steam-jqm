6aac8607bd78899919d6ba6a25402a02ae25b8a3
Jacob Mumm <grendian@gmail.com>
2013-02-05 19:47:40 -0500

    code.js

---
+++ code.js
@@ -0,0 +1,6 @@
+$(function(){
+    $.getJSON("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=B288A76BB1621D6D9F02D6DA5DC54454&steamid=76561197960432907",
+        function(data){
+           console.log(data); 
+        });    
+});
