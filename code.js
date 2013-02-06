b9209423c6a132062dc94db4337473012b71753c
Jacob Mumm <grendian@gmail.com>
2013-02-05 20:16:25 -0500

    list attempt #1

--- code.js
+++ code.js
@@ -3,5 +3,11 @@ $(function(){
            console.log(data); 
            
            $("#gameName").text(data.playerstats.gameName);
+           
+           for(var i=0; i<data.playerstats.achievements.length;i++){
+               var newItem = $('<li></li>');
+               newItem.text(data.playerstats.achievements[i].apiname);
+               $("#achievementList").append(newItem);
+           }
         });
 });
