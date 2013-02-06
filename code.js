1f5a179894ce3148ed8e42111a9aebca4afff648
Jacob Mumm <grendian@gmail.com>
2013-02-05 20:03:37 -0500

    achievements.json

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 $(function(){
-    $.getJSON("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=B288A76BB1621D6D9F02D6DA5DC54454&steamid=76561197960432907&jsoncallback=?",
-        {format: "json"}, function(data){
+    $.getJSON("achievements.json", function(data){
            console.log(data); 
         });    
 });
